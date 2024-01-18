1bb14fef30fa1e07c08db285dd5e1d66b65bd43f
Pieter van Ginkel <pvginkel@gmail.com>
2024-01-18 13:26:23 +0100

    Created new thermostat project.

---
+++ Thermostat/Thermostat.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Thermostat.ino
+ Created:	1/18/2024 1:24:29 PM
+ Author:	pvgin
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
